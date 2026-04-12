b5265209b26643000aaad7e58f72b83030e2ae08
Simon Saez <simon.saez@usach.cl>
2026-04-11 23:28:24 -0400

    Limpieza

--- src/features/templates/index.ts
+++ src/features/templates/index.ts
@@ -1,5 +1,2 @@
-export { TemplatePanel } from './components/TemplatePanel/TemplatePanel';
 export { ExportModal } from './components/ImportExportModal/ExportModal';
 export { ImportModal } from './components/ImportExportModal/ImportModal';
-export { useTemplateStore } from './hooks/useTemplateStore';
-export type { Template, TemplateStore } from './hooks/useTemplateStore';
